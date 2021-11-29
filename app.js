9f2591d7ff37fcc1658ef669096c59859f258d89
Rutvik Umak <rutvikumak@gmail.com>
2021-11-29 22:46:15 +0530

    feat: Added function , styling and formatting

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
 const dateOfBirth = document.querySelector("#birth-date")
-const luckyNo =  document.querySelector("#lucky-no");
+const luckyNo = document.querySelector("#lucky-no");
 const submitBtn = document.querySelector(".submit");
+const resetBtn = document.querySelector(".reset");
 const output = document.querySelector(".output");
+const errorMsg = document.querySelector(".error");
 
-submitBtn.addEventListener("click",()=>{
-    const date= dateOfBirth.value.replaceAll('-','').split('');
-    let sumOfDates =0;
+submitBtn.addEventListener("click", () => {
+    errorMsg.style.display = ""
 
-    for(let no of date){
-        sumOfDates+= parseInt(no);
+    if (!parseInt(luckyNo.value) && dateOfBirth.value === "") {
+        errorMsg.innerText = "Input not provided";
+    } else if (!parseInt(luckyNo.value)) {
+        errorMsg.innerText = "Please enter your lucky number"
+    } else if (dateOfBirth.value === "") {
+        errorMsg.innerText = "Please enter your Date of Birth"
+    } else {
+        errorMsg.innerText = ""
+        const date = dateOfBirth.value.replaceAll('-', '').split('');
+        let sumOfDates = 0;
+
+        for (let no of date) {
+            sumOfDates += parseInt(no);
+        }
+        if (Math.floor(sumOfDates % luckyNo.value) === 0) {
+            output.innerText = `${luckyNo.value} is Lucky Number ❤️`;
+
+        } else {
+            output.innerText =  `${luckyNo.value} is not that Lucky 😶`
+
+        }
     }
+})
 
-    if(Math.floor(sumOfDates%luckyNo.value) === 0){
-        output.innerText = "Lucky"
-    }else{
-        output.innerText = "Not Lucky"
-
-    }
+resetBtn.addEventListener("click", () => {
+    luckyNo.value = "";
+    dateOfBirth.value = ""
+    output.innerText = ""
+    errorMsg.innerText = ""
 })
